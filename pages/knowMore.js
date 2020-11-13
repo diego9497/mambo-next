6e71642591897023544f01d571f38ff78e44a7ff
Diego Aguilera <diego@loducode.com>
2020-11-13 11:02:40 -0500

    exportar página del saber más

--- pages/knowMore.js
+++ pages/knowMore.js
@@ -3,7 +3,7 @@ import GlobalStyles from "../styles/GlobalStyles";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-const KnowMore = ({ config }) => {
+export default KnowMore = ({ config }) => {
   return (
     <>
       <Head>
