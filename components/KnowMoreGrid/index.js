3157d2d0d596511eec340e2a2348b8e3680d7a32
Camilo Aguilera <camilo9497@live.com>
2020-11-19 12:14:08 -0500

    vista de conoce mas hecha sin responsive ni lineas

--- components/KnowMoreGrid/index.js
+++ components/KnowMoreGrid/index.js
@@ -1,7 +1,56 @@
-import {} from './style'
+import {
+  Container,
+  ContainerSection,
+  Title,
+  ContainerInfo,
+  ImageBackground,
+  Text,
+  ContentInfo,
+} from "./styles";
 
 function KnowMoreGrid(props) {
-  return(
-
-  )
+  let color = "#335FD7";
+  return (
+    <Container>
+      <ContainerSection>
+        <Title color={color}>Conoce más</Title>
+      </ContainerSection>
+      <ContainerSection color={color}>
+        <ContentInfo>IG @mambogota</ContentInfo>
+        <ContentInfo>FB @fanmambogota</ContentInfo>
+        <ContentInfo>TW @_mambogota</ContentInfo>
+        <ContentInfo>YT Museo de Arte Moderno de Bogotá</ContentInfo>
+      </ContainerSection>
+      <ContainerSection>
+        <ImageBackground src="/image/3.jpg" />
+        <ContainerInfo color={color}>
+          <Text>Oscar Monsalve</Text>
+          <Text>Fuente archivo MAMBO</Text>
+        </ContainerInfo>
+      </ContainerSection>
+      <ContainerSection>
+        <ImageBackground src="/image/3.jpg" />
+        <ContainerInfo color={color}>
+          <Text>Oscar Monsalve</Text>
+          <Text>Fuente archivo MAMBO</Text>
+        </ContainerInfo>
+      </ContainerSection>
+      <ContainerSection>
+        <ImageBackground src="/image/3.jpg" />
+        <ContainerInfo color={color}>
+          <Text>Oscar Monsalve</Text>
+          <Text>Fuente archivo MAMBO</Text>
+        </ContainerInfo>
+      </ContainerSection>
+      <ContainerSection>
+        <ImageBackground src="/image/3.jpg" />
+        <ContainerInfo color={color}>
+          <Text>Oscar Monsalve</Text>
+          <Text>Fuente archivo MAMBO</Text>
+        </ContainerInfo>
+      </ContainerSection>
+    </Container>
+  );
 }
+
+export default KnowMoreGrid;
--- components/KnowMoreGrid/styles.js
+++ components/KnowMoreGrid/styles.js
@@ -1,1 +1,78 @@
 import styled from "styled-components";
+
+export const Container = styled.div`
+  display: grid;
+  height: calc(100vh - var(--headerHeight) * 2);
+  grid-template-columns: repeat(3, 1fr);
+  grid-template-rows: 1fr 1fr;
+  grid-template-areas:
+    "Title Information EmptyO"
+    "Author EmptyTw EmptyTh";
+  font-size: 17px;
+`;
+export const ContainerSection = styled.div`
+  position: relative;
+  :hover {
+    > div {
+      display: initial;
+    }
+  }
+  :nth-child(1) {
+    grid-area: Title;
+    padding: 30px;
+  }
+  :nth-child(2) {
+    background: red;
+    grid-area: Information;
+    padding: 20px;
+    background: ${(props) => props.color};
+    color: white;
+  }
+  :nth-child(3) {
+    grid-area: EmptyO;
+  }
+  :nth-child(4) {
+    grid-area: Author;
+  }
+  :nth-child(5) {
+    grid-area: EmptyTw;
+  }
+  :nth-child(6) {
+    grid-area: EmptyTh;
+  }
+`;
+export const Title = styled.p`
+  font-size: 4em;
+  width: 50%;
+  line-height: 50px;
+  color: ${(props) => props.color};
+  font-weight: 300;
+`;
+
+export const ImageBackground = styled.img`
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  filter: grayscale(1);
+  object-fit: cover;
+`;
+export const ContainerInfo = styled.div`
+  color: white;
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  padding: 20px 20px 40px 20px;
+  background: blue;
+  display: none;
+  background: ${(props) => props.color};
+`;
+export const Text = styled.p`
+  font-weight: 100;
+  :nth-child(1) {
+    font-weight: 400;
+  }
+`;
+export const ContentInfo = styled.p`
+  font-weight: 300;
+  font-size: 1em;
+`;
