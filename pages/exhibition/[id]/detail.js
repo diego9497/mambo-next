16a050d5765eacb1d655df94041c5794c22e7b35
Diego Aguilera <diego@loducode.com>
2020-11-23 12:55:19 -0500

    Feature: Añadir carousel que se mueve

--- pages/exhibition/[id]/detail.js
+++ pages/exhibition/[id]/detail.js
@@ -12,6 +12,21 @@ const Detail = ({ exhibition, config }) => {
     <>
       <Head>
         <title>{exhibition.title} | MAMBO Viajero</title>
+        {/* <link
+          rel="stylesheet"
+          href="https://unpkg.com/react-slidy/lib/styles.css"
+        /> */}
+        <link
+          rel="stylesheet"
+          type="text/css"
+          charset="UTF-8"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+        />
+        <link
+          rel="stylesheet"
+          type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+        />
       </Head>
       <Exhibition exhibition={exhibition} config={config} />
     </>
