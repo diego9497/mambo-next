f78a0bf6f2c229cfff764c5b3db9e664c991d119
Diego Aguilera <diego@loducode.com>
2020-11-30 18:11:16 -0500

    Chore: Información exhibicion 5

--- components/ExhibitionPrimary/index.js
+++ components/ExhibitionPrimary/index.js
@@ -33,7 +33,6 @@ export default function ExhibitionPrimary({ exhibition, config }) {
     author,
     authorContent,
     authorAudio,
-    gallery,
     keyConcepts,
     didYouKnowImg,
     activityImg,
